2aa4ee0cfddb2cd2fe8bc01173f0dda3e11da0aa
Dhruvil Nakrani <128470872+nd5a@users.noreply.github.com>
2024-12-07 17:33:53 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Model from "./components/Model";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
+import SmoothScrolling from "./components/SmoothScrolling";
+
+const App = () => {
+  return (
+    <SmoothScrolling>
+      <main className="bg-black overflow-y-auto">
+        <Navbar />
+        <Hero />
+        <Highlights />
+        <Model />
+        <Features />
+        <HowItWorks />
+        <Footer />
+      </main>
+    </SmoothScrolling>
+  );
+};
+
+export default App;
